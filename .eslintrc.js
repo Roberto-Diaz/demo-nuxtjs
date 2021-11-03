e0cd4e69c5548c6cee331702e7d03f8d4af1c671
Roberto Diaz Diaz <robertodd07@gmail.com>
2021-11-02 18:11:12 -0600

    feat: post component

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,6 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
-    "no-console": "off",
+    'no-console': 'off'
   }
 }
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,29 +1,22 @@
 <template>
-  <div>
-    <div class="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-3 gap-4 gap-4">
-      <div class="bg-gray-100 p-2">
-        <p class="text-center">
-          titulo
-        </p>
-        <p class="text-justify">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam recusandae magnam sapiente sint aspernatur! Nulla, cum molestiae? Quisquam, quasi. Modi odit harum iure autem omnis quaerat! Porro excepturi exercitationem pariatur.
-        </p>
-      </div>
-      <div class="bg-gray-100 p-2">
-        <p class="text-center">
-          titulo
-        </p>
-        <p class="text-justify">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam recusandae magnam sapiente sint aspernatur! Nulla, cum molestiae? Quisquam, quasi. Modi odit harum iure autem omnis quaerat! Porro excepturi exercitationem pariatur.
-        </p>
-      </div>
-    </div>
+  <div class="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-3 gap-4 gap-4">
+    <Post
+      v-for="(post, index) in posts"
+      :id="post.id"
+      :key="index"
+      :title="post.title"
+      :body="post.body"
+    />
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import Post from '../../components/Post.vue'
 export default {
+  components: {
+    Post
+  },
   data () {
     return {
       posts: []
